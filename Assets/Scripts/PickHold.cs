7dd9624f40effabdec7027a6ad71a87e8cbc8ee5
Tristan Barrett <1006459@student.sae.edu.au>
2016-10-26 22:19:38 +1000

    Lift animated, some prefabs made, sudo script scripted
    overall super behind in making game its due in 2 days and its not playable

---
+++ Assets/Scripts/PickHold.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickHold : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if E button is clicked
+        //send raycast infront of player
+        //if object is hit is marked as carryable 
+        //and if in range
+        //move object to player hands (empty gameobject)
+        //turn off rigidbody and make child
+
+        //if E is pressed again release object 
+            //release child
+            //turn on rigidbody, put is state that is was before picking up
+	}
+}
---
+++ Assets/Scripts/PlayerGun.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    //Controls what the gun does and what buttons control it
+    
+    //if left mouse clicked 
+        //send raycast
+        //turn off objects gravity in rigidbody
+        //if clicked again
+        //reverse gravity option
+            //if on turn off/ vice versa
+    //if right mouse is clicked
+        //send raycast
+        //lock into position, ?turn off rigidbody
+        //if clicked again
+        //unlock object
+            //reverse the lock and unlock
+}
